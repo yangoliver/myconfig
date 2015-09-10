17dd1b02d2c283acb265efc3e7c1e81960ff8d0f
Yong Yang <yangoliver@gmail.com>
2015-09-10 07:46:45 -0400

    check local config by diff
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

---
+++ check_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -f ~/.bashrc ]; then
+	diff -u .bashrc ~/.bashrc
+fi
+
+if [ -f ~/cs_bld.sh ]; then
+	diff -u cs_bld.sh ~/cs_bld.sh
+fi
+
+if [ -f ~/.vim/spell/en.utf-8.add ]; then
+	diff -u en.utf-8.add ~/.vim/spell/en.utf-8.add
+fi
+
+if [ -f ~/.gitconfig ]; then
+	diff -u  .gitconfig ~/.gitconfig
+fi
+
+if [ -f ~/.msmtprc ]; then
+	diff -u .msmtprc ~/.msmtprc
+fi
+
+if [ -f ~/.screenrc ]; then
+	diff -u .screenrc ~/.screenrc
+fi
+
+if [ -f ~/.vimrc ]; then
+	diff -u .vimrc ~/.vimrc
+fi
