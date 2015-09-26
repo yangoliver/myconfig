b96983cee93fdc2b54b6d961fb37477e2235a043
Yong Yang <yangoliver@gmail.com>
2015-09-26 08:32:42 -0400

    setup markdown vim syntax highlight
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

---
+++ md_vim_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#cd /dev/shm && wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz
+#cd ~/.vim && tar --strip=1 -zxf /dev/shm/master.tar.gz
+#rm -f /dev/shm/master.tar.gz
+
+grep vim_markdown_folding_disabled ~/.vimrc 
+if [ $? -eq 1 ]; then
+	echo "let g:vim_markdown_folding_disabled=1" >> ~/.vimrc
+fi
