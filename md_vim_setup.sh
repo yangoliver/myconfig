a1ac52e15d991ec9e2b0855d0310efb5fd630995
Yong Yang <yangoliver@gmail.com>
2015-09-26 08:35:39 -0400

    uncomment the code
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

--- md_vim_setup.sh
+++ md_vim_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#cd /dev/shm && wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz
-#cd ~/.vim && tar --strip=1 -zxf /dev/shm/master.tar.gz
-#rm -f /dev/shm/master.tar.gz
+cd /dev/shm && wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz
+cd ~/.vim && tar --strip=1 -zxf /dev/shm/master.tar.gz
+rm -f /dev/shm/master.tar.gz
 
 grep vim_markdown_folding_disabled ~/.vimrc 
 if [ $? -eq 1 ]; then
