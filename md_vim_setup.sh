a59393e44e575558ef22e9c44fb47ef3b1af8d20
Yong Yang <yangoliver@gmail.com>
2016-01-10 15:44:54 +0800

    fix a minor bug when .vim dir is not created at home dir

--- md_vim_setup.sh
+++ md_vim_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd /dev/shm && wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz
+if [ ! -d ~/.vimi ]; then
+	mkdir ~/.vim
+fi
 cd ~/.vim && tar --strip=1 -zxf /dev/shm/master.tar.gz
+
 rm -f /dev/shm/master.tar.gz
 
 grep vim_markdown_folding_disabled ~/.vimrc 
