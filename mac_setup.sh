f09a79446ff7d0f5151e7bcd973fdad77e535cde
Yong Yang <yangoliver@gmail.com>
2016-01-29 21:43:51 +0800

    added new mac setup file for mac os setup

---
+++ mac_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -f ~/.bash_profile ]; then
+	cp ~/.bash_profile ~/.bash_profile.orig
+fi
+# mac os use .bash_profile because the bash gets invoked via login way
+cp .bash_rc ~/.bash_profile
+
+cp cs_bld.sh ~/cs_bld.sh
+
+if [ -f ~/.gitconfig ]; then
+	cp ~/.gitconfig ~/.gitconfig.orig
+fi
+cp .gitconfig ~/.gitconfig
+
+if [ -f ~/.msmtprc ]; then
+	cp ~/.msmtprc ~/.msmtprc.orig
+fi
+cp .msmtprc ~/.msmtprc
+
+if [ -f ~/.screenrc ]; then
+	cp ~/.screenrc ~/.screenrc.orig
+fi
+cp .screenrc ~/.screenrc
+
+if [ -f ~/.vimrc ]; then
+	cp ~/.vimrc ~/.vimrc.orig
+fi
+cp .vimrc ~/.vimrc
+
+if [ -f ~/.vim/spell/en.utf-8.add ]; then
+	cp ~/.vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add.orig
+	cp en.utf-8.add ~/.vim/spell/en.utf-8.add
+fi
