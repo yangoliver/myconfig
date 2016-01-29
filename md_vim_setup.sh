a0b6d5133fe128c1e79fde2232b01ac34aca5a6a
Yong Yang <yangoliver@gmail.com>
2016-01-29 22:43:48 +0800

    make it work with mac os: wget replaced by curl and check tmp dir

--- md_vim_setup.sh
+++ md_vim_setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-cd /dev/shm && wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz
-if [ ! -d ~/.vimi ]; then
+if [ ! -d /dev/shm ]; then
+	if [ ! -d /tmp ]; then
+		echo "Can't determine tmp dir, is it a Linux/Unix"
+		exit 1
+	fi
+	TMP_DIR=/tmp
+else
+	TMP_DIR=/dev/shm
+fi
+
+cd ${TMP_DIR} && curl -O -L https://github.com/plasticboy/vim-markdown/archive/master.tar.gz
+
+if [ ! -d ~/.vim ]; then
 	mkdir ~/.vim
 fi
-cd ~/.vim && tar --strip=1 -zxf /dev/shm/master.tar.gz
 
-rm -f /dev/shm/master.tar.gz
+cd ~/.vim && tar --strip=1 -zxf ${TMP_DIR}/master.tar.gz
+rm -f ${TMP_DIR}/master.tar.gz
 
 grep vim_markdown_folding_disabled ~/.vimrc 
 if [ $? -eq 1 ]; then
